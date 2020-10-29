c74b4d49a2fb272b0bcf81adf4ba8a75c9c9a564
sammycoderu <sambiiaws@gmail.com>
2020-10-29 17:55:13 -0400

    initial codes added

---
+++ vars.tf
@@ -0,0 +1,31 @@
+variable "AWS_ACCESS_KEY" {
+}
+
+variable "AWS_SECRET_KEY" {
+}
+
+variable "AWS_REGION" {
+  default = "us-east-1"
+}
+
+variable "AMIS" {
+  type = map(string)
+  default = {
+    us-east-1 = "ami-0dba2cb6798deb6d8"
+    us-west-1 = "ami-021809d9177640a20"
+    us-west-2 = "ami-06e54d05255faf8f6"
+  }
+}
+
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "mykey"
+}
+
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "mykey.pub"
+}
+
+variable "INSTANCE_USERNAME" {
+  default = "ubuntu"
+}
+
